85acbb001c2c70340036fd687b715ba0d25c5eff
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-17 23:48:39 +0000

    Code tidying

--- task/index.php
+++ task/index.php
@@ -5,14 +5,14 @@ function index($data): string
     return APIResponse("Hello, " . $data['queryStringParameters']['name']);
 }
 
-function APIResponse($body): string
+function APIResponse(string $body): string
 {
-    $headers = array(
-        "Content-Type"=>"application/json",
-        "Access-Control-Allow-Origin"=>"*",
-        "Access-Control-Allow-Headers"=>"Content-Type",
-        "Access-Control-Allow-Methods" =>"OPTIONS,POST"
-    );
+    $headers = [
+        "Content-Type" => "application/json",
+        "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Headers" => "Content-Type",
+        "Access-Control-Allow-Methods" => "OPTIONS,POST"
+    ];
     return json_encode([
         "statusCode" => 200,
         "headers" => $headers,
