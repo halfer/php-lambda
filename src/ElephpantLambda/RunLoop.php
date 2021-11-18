1ea03e0fb6fb732faa7e4d736c8a9b71321abec7
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-18 20:18:23 +0000

    Sketch out rough structure of runloop object

--- src/ElephpantLambda/RunLoop.php
+++ src/ElephpantLambda/RunLoop.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace ElephpantLambda;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class RunLoop
+{
+    public function __construct(protected GuzzleClient $client, protected string $runtimeHost)
+    {
+    }
+
+    public function runLoop(): void
+    {
+
+    }
+
+    protected function getNextRequest(): array
+    {
+        return [];
+    }
+
+    /**
+     * @todo What is the type of the invocationId? - string?
+     * @param $invocationId
+     * @param string $response
+     */
+    protected function sendResponse($invocationId, string $response): void
+    {
+
+    }
+}
