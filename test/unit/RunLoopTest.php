951180bdbdff96c0682c30f6e0ff071644afa924
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-18 20:43:23 +0000

    Propose some tests for the main run loop

---
+++ test/unit/RunLoopTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class RunLoopTest extends TestCase
+{
+    public function testRunLoopOnce()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRunLoopFunctionNotInScope()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRunLoopMissingPayload()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRunLoopMissingInvocationId()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetRequestHttpFault()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSendResponseHttpFault()
+    {
+        $this->markTestIncomplete();
+    }
+}
