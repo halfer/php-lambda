da4b75fd04072ff30a8c9351e08280fea9d6f435
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-18 20:34:45 +0000

    Sketch out some abstract lambda code

--- src/ElephpantLambda/Lambda.php
+++ src/ElephpantLambda/Lambda.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace ElephpantLambda;
+
+abstract class Lambda
+{
+    abstract function run(array $data): string;
+
+    protected function apiResponse(string $body): string
+    {
+        $headers = [
+            "Content-Type" => "application/json",
+            "Access-Control-Allow-Origin" => "*",
+            "Access-Control-Allow-Headers" => "Content-Type",
+            "Access-Control-Allow-Methods" => "OPTIONS,POST"
+        ];
+        return json_encode([
+            "statusCode" => 200,
+            "headers" => $headers,
+            "body" => $body
+        ]);
+    }
+}
