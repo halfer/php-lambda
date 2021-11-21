0e546cb538f927c6506db7890806670fc13bb491
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-21 23:38:33 +0000

    Initial generation of a CDK project

---
+++ cdk/test/src2.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Src2 from '../lib/src2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/src2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Src2.Src2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
