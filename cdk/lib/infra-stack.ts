cfaa7ee5b5a82902dd491d431e44662e1ba55149
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-26 00:17:31 +0000

    Add a security group port allowance

--- cdk/lib/infra-stack.ts
+++ cdk/lib/infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as ecr from "@aws-cdk/aws-ecr";
 import * as ec2 from "@aws-cdk/aws-ec2"
+//import * as efs from "@aws-cdk/aws-efs"
 import {Handler} from "@aws-cdk/aws-lambda";
 import * as fs from 'fs';
 
@@ -24,16 +25,28 @@ export class InfraStack extends cdk.Stack {
         `arn:aws:ecr:eu-west-2:${account}:repository/php-lambda`
     );
 
-    const vpc = new ec2.Vpc(this, "LambdaVpc", {
-
+    const network = new ec2.Vpc(this, "LambdaVpc", {
     });
+
+    const sg = new ec2.SecurityGroup(this, "SecurityGroup", {
+      vpc: network,
+      allowAllOutbound: false,
+      securityGroupName: "PermitNfsTraffic",
+    });
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(2049));
+    sg.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(2049));
+
+    // const fileSystem = new efs.FileSystem(this, "Efs", {
+    //   vpc: network,
+    //   encrypted: true,
+    // });
 
     const func = new lambda.Function(this, "DockerFunction", {
       runtime: lambda.Runtime.FROM_IMAGE,
       code: lambda.Code.fromEcrImage(repo),
       handler: Handler.FROM_IMAGE,
       securityGroups: [],
-      //vpc: vpc
+      //vpc: network,
     });
   }
 }
