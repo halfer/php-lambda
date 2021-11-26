a5738dd7a5c8c9ebf439b94d8ab37c450d01157b
Jon <projects.ws{symbol}jondh.me.uk>
2021-11-26 12:41:54 +0000

    Add comments to CDK script

--- cdk/lib/infra-stack.ts
+++ cdk/lib/infra-stack.ts
@@ -10,13 +10,8 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Plan:
+    // Still need to add:
     //
-    // * A lambda based on a Docker image
-    // * Permissions on the lambda
-    // * EFS
-    // * A VPC to connect between EFS & lambda
-    // * A security group to poke a hole for NFS
     // * EventBridge cron
     // * A lifecycle for ECR to stop old image stockpiling
 
@@ -25,9 +20,8 @@ export class InfraStack extends cdk.Stack {
         `arn:aws:ecr:eu-west-2:${account}:repository/php-lambda`
     );
 
-    const network = new ec2.Vpc(this, "LambdaVpc", {
-    });
-
+    // This pokes a port hole for NFS to work
+    const network = new ec2.Vpc(this, "LambdaVpc");
     const sg = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc: network,
       allowAllOutbound: false,
@@ -36,6 +30,7 @@ export class InfraStack extends cdk.Stack {
     sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(2049));
     sg.addEgressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(2049));
 
+    // Here's the file system to connect to the lambda
     const fileSystem = new efs.FileSystem(this, "Efs", {
       vpc: network,
       encrypted: true,
@@ -45,6 +40,7 @@ export class InfraStack extends cdk.Stack {
       posixUser: { uid: '0', gid: '0', }
     });
 
+    // And here's the lambda
     const func = new lambda.Function(this, "DockerFunction", {
       runtime: lambda.Runtime.FROM_IMAGE,
       code: lambda.Code.fromEcrImage(repo),
